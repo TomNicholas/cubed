61aa96c0c75f0a1d07e6e7327d15072c98f765e2
GitHub Action <action@github.com>
2023-07-13 16:31:39 +0000

    Add changes for 01f8eb923feeb69ea74f39432572541825469054

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0110a40',
+    VERSION: '01f8eb9',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
